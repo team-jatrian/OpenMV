0ec6a0b0e9a870c51574789d269e0bfafd79bfff
sin-alpha-pi <53899103+sin-alpha-pi@users.noreply.github.com>
2020-01-24 14:20:32 +0100

    Update CenterOnLine.py

--- CenterOnLine.py
+++ CenterOnLine.py
@@ -2,35 +2,49 @@
 import sensor, image, pyb
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
-sensor.set_framesize(sensor.QQQVGA)
+sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 
 uart = pyb.UART(3, 115200, timeout_char=1000, bits=8, parity=1, stop=2)
 
 threshold_line = [(0, 33, -128, 127, -128, 127)]
 
-roi_top = [0, 0, 80, 20]
-roi_middle = [0, 20, 80, 20]
-roi_bottom= [0, 40, 80, 20]
+width = sensor.width()
+height = sensor.height()
 
 accuracy = 3
 rois = [None] * accuracy
 xVals = [None] * accuracy
-
+all_blobs_N = [None] * accuracy
+yVals = [None]
 while(1):
    img = sensor.snapshot()
    def splitter():
        for roi in enumerate(rois):
+            global all_blobs
+
             loop_index = roi[0]
-            width = 80
-            general_height = int(60/len(rois))
+            general_height = int(height/len(rois))
             single = loop_index * general_height
             xVals[loop_index] = single
-            rois[loop_index] = [0, xVals[loop_index], 80, general_height]
+            rois[loop_index] = [0, xVals[loop_index], width, general_height]
             blobs = img.find_blobs(threshold_line, False, rois[loop_index])
-            img.draw_rectangle(rois[loop_index])
-            print(len(blobs))
-        for blob in blobs:
-            img.draw_rectangle(blob.x(), blob.y(), blob.w(), blob.y())
+            all_blobs_N[loop_index] = blobs
 
+            img.draw_rectangle(rois[loop_index])
+            for blob in blobs:
+                img.draw_rectangle(blob.rect())
+
+            all_blobs = []
+            for val in all_blobs_N:
+                 if val != []:
+                     all_blobs.append(val)
+
+       for i in range(len(all_blobs)):
+           global yVals
+           yVals = [None] * len(all_blobs)
+           yVals[i] = all_blobs[i][0].y()
+       ["HEllo" if v is None else v for v in yVals]
+       print(yVals)
+       #lowest_y = min(yVals)
    splitter()
